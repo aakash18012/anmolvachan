f4d62981269815f54653d0f1dbecef4a24a91eeb
aakash1801 <singh.aakash1801@gmail.com>
2019-05-03 17:40:31 +0530

    remove localhost

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class QuoteService {
 
-  uri = 'http://localhost:8080/quotes';
+  uri = 'quotes';
 
   constructor(private http: HttpClient) { }
   getQuotes(quoteType) {
